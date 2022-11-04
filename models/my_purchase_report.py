5281d950990cf69cc125e109c1efb6c72f405fd5
Anh Tuan <silerspirit2001@gmail.com>
2022-11-04 08:55:55 +0700

    api

---
+++ models/my_purchase_report.py
@@ -0,0 +1,32 @@
+from odoo import fields, models, api
+from datetime import datetime
+
+
+class PurchaseReport(models.Model):
+    _name = 'purchase.report'
+    _description = 'Description'
+
+    department_id = fields.Many2one('hr.department')
+    real_cost = fields.Float(compute='_compute_real_cost')
+    target_diff = fields.Float(compute='_compute_target_diff')
+    cron_id = fields.Many2one('my.cron')
+
+    @api.depends('department_id')
+    def _compute_real_cost(self):
+        for dep in self:
+            def filter_month(f_rec):
+                return f_rec.create_date.month == datetime.today().month
+
+            opportunity_data = self.env['purchase.order'].search(
+                [('state', '=', 'purchase'), ('department_id', '=', dep.department_id.id)]).filtered(
+                filter_month)
+            if len(opportunity_data) == 0:
+                dep.real_cost = 0
+            else:
+                for rec in opportunity_data:
+                    dep.real_cost += sum(rec.mapped('amount_total'))
+
+    @api.depends('real_cost', 'department_id.monthly_spending_limit')
+    def _compute_target_diff(self):
+        for dep in self:
+            dep.target_diff = dep.real_cost - dep.department_id.monthly_spending_limit
---
+++ models/my_sale_report.py
@@ -0,0 +1,32 @@
+from odoo import fields, models, api
+from datetime import datetime
+
+
+class SaleReport(models.Model):
+    _name = 'sale.report'
+    _description = 'Description'
+
+    sale_team_id = fields.Many2many('crm.team')
+    real_revenue = fields.Float(compute='_compute_real_revenue')
+    sale_diff = fields.Float(compute='_compute_sale_diff')
+    cron_id = fields.Many2one('my.cron')
+
+    @api.depends('sale_team_id')
+    def _compute_real_revenue(self):
+        for team in self:
+            def filter_month(f_rec):
+                return f_rec.create_date.strftime('%b') == datetime.today().month
+
+            opportunity_data = self.env['crm.lead'].search(
+                [('type', '=', 'opportunity'), ('team_id.id', 'in', team.sale_team_id.mapped('id'))]).filtered(
+                filter_month)
+            if len(opportunity_data) == 0:
+                team.real_revenue = 0
+            else:
+                for rec in opportunity_data:
+                    team.real_revenue += sum(rec.order_ids.mapped('amount_total'))
+
+    @api.depends('real_revenue', 'sale_team_id.invoiced_target')
+    def _compute_sale_diff(self):
+        for team in self:
+            team.sale_diff = team.real_revenue - team.sale_team_id.invoiced_target
